08f451d10dd9578b68e17e3bdd12dc91a0362211
tracer-nju <tracer@nju.edu.cn>
2023-10-31 10:30:43 +0800

    Linux LAPTOP-6BH4BGNM 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:30:43 up  1:42,  0 users,  load average: 0.01, 0.09, 0.06

--- amgame/src/keyboard.c
+++ amgame/src/keyboard.c
@@ -9,6 +9,7 @@ void print_key()
 {
   AM_INPUT_KEYBRD_T event = {.keycode = AM_KEY_NONE};
   ioe_read(AM_INPUT_KEYBRD, &event);
+  puts("Key pressed: ");
   if (event.keycode != AM_KEY_NONE && event.keydown)
   {
     puts("Key pressed: ");
