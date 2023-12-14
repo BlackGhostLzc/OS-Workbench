63cde2e399043fc70e069bd815e63f7da253a1de
tracer-nju <tracer@nju.edu.cn>
2023-12-14 23:40:28 +0800

    Linux LAPTOP-6BH4BGNM 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:40:28 up  4:38,  0 users,  load average: 0.13, 0.26, 0.24

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -5,12 +5,12 @@
 int main(const char *args)
 {
   ioe_init();
-  printf("Hello World from " __ISA__ " program!\n");
+  // printf("Hello World from " __ISA__ " program!\n");
   puts("mainargs = \"");
   puts(args); // make run mainargs=xxx
   puts("\n");
 
-  splash();
+  // splash();
 
   puts("Press any key to see its key code...\n");
   while (1)
