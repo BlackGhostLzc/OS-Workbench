08a75d11402c6bc2b547a901bace4561e7968971
tracer-nju <tracer@nju.edu.cn>
2023-12-24 19:13:57 +0800

    Linux LAPTOP-6BH4BGNM 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:13:57 up  3:22,  0 users,  load average: 0.11, 0.13, 0.24

--- libco/co.c
+++ libco/co.c
@@ -96,7 +96,7 @@ static Co_node *remove_node()
 struct co *co_start(const char *name, void (*func)(void *), void *arg)
 {
   struct co *new_co = (struct co *)malloc(sizeof(struct co));
-  new_co->arg = arg;
+  new_co->arg = (char *)arg;
   new_co->func = func;
   new_co->name = name;
   new_co->status = CO_NEW;
