f547e9e56a01f319d210843c19d8af95380b62b0
tracer-nju <tracer@nju.edu.cn>
2024-01-11 11:38:49 +0800

    Linux LAPTOP-6BH4BGNM 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:38:49 up  2:31,  0 users,  load average: 0.10, 0.08, 0.07

--- kernel/framework/main.c
+++ kernel/framework/main.c
@@ -1,9 +1,10 @@
 #include <kernel.h>
 #include <klib.h>
 
-int main() {
+int main()
+{
   ioe_init();
-  cte_init(os->trap);
+  // cte_init(os->trap);
   os->init();
   mpe_init(os->run);
   return 1;
