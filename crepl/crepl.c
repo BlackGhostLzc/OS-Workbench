78bbca2e76a1ca7eea7a59a7867eae7a4f6c8822
tracer-nju <tracer@nju.edu.cn>
2024-01-11 10:28:19 +0800

    Linux LAPTOP-6BH4BGNM 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:28:19 up  1:20,  0 users,  load average: 0.59, 0.53, 0.34

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -70,7 +70,7 @@ int main(int argc, char *argv[])
       printf("%d\n", len);
       if (line[strlen(line)] == '\n')
       {
-        line[strlen(line)] == '\0';
+        line[strlen(line)] == ' ';
       }
 
       FILE *f2 = fopen(libc_name, "a");
--- crepl/tmp/lib.c
+++ crepl/tmp/lib.c
@@ -6,3 +6,5 @@ int wrapper_0() { return f()
 ; }
 int wrapper_0() { return h()
 ; }
+int wrapper_0() { return n()
+; }
