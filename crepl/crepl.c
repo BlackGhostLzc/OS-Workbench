dc7aa78de33a4d918e9ca71b8ef70194c3d17e88
tracer-nju <tracer@nju.edu.cn>
2024-01-11 09:46:32 +0800

    Linux LAPTOP-6BH4BGNM 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:46:32 up 39 min,  0 users,  load average: 0.26, 0.35, 0.26

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -2,13 +2,38 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <dlfcn.h>
+#include <sys/wait.h>
+
+char libc_name[] = "./tmp/lib.c";
+char libso_name[] = "./tmp/lib.so";
+char *gcc_option[] = {
+    "-shared",
+    "-fPIC",
+    libc_name,
+    "-o",
+    libso_name,
+    NULL,
+};
+
+void update_lib()
+{
+  if (fork() == 0)
+  {
+    execvp("gcc", gcc_option);
+    perror("gcc error");
+    exit(-1);
+  }
+  wait(NULL);
+}
+
+int wrapper_num = 0;
 
 int main(int argc, char *argv[])
 {
   static char line[4096];
 
-  char filename[] = "/tmp/lib.c";
-  int fd = mkstemp(filename);
+  int (*wrapper)();
 
   while (1)
   {
@@ -18,8 +43,44 @@ int main(int argc, char *argv[])
     {
       break;
     }
-    write(fd, line, strlen(line));
 
-    printf("Got %zu chars.\n", strlen(line)); // ??
+    if (strlen(line) > 3 && strncmp(line, "int", 3) == 0)
+    {
+      // 是个函数申明
+      // int func_name(int a, ...) {....; return x;}
+      FILE *f = fopen(libc_name, 'a');
+      fprintf(f, "\n%s\n", line);
+      fclose(f);
+      printf("Got %zu chars.\n", strlen(line)); // ??
+    }
+    else
+    {
+      // 这是一个表达式.需要构造一个wrapper， wrapper_0  wrapper_1
+      char wrapper_buf[1024];
+      FILE *f = fopen(libc_name, "a");
+      fprintf(f, "%s%s%d%s%s%s",
+              "int ", "wrapper_", wrapper_num, "(){ return ", line, "; }\n");
+
+      update_lib();
+
+      void *libHandle = dlopen(libso_name, RTLD_LAZY);
+      if (!libHandle)
+      {
+        printf("handle error\n");
+        continue;
+      }
+
+      sprintf(wrapper_buf, "%s%d", "wrapper_", wrapper_num);
+      wrapper = (int (*)())dlsym(libHandle, wrapper_buf);
+      if (!wrapper)
+      {
+        printf("wrapper error\n");
+      }
+
+      int res = wrapper();
+
+      printf("%d\n", res);
+      wrapper_num++;
+    }
   }
 }
