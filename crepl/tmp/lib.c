a22e056819f9770ee194445b615a2b090974e3fc
“BlackGhostLzc” <2427324519@qq.com>
2024-01-11 10:58:15 +0800

    crepl is finished

--- crepl/tmp/lib.c
+++ crepl/tmp/lib.c
@@ -1,1 +1,11 @@
 
+int fib(int n) { if (n <= 1) return 1; return fib(n - 1) + fib(n - 2); }
+
+int wrapper_0() { return fib(5); }
+int wrapper_1() { return fib(6); }
+int wrapper_2() { return fib(5)+fib(6)*2; }
+
+int add(int a, int b){return a + b;}
+
+int wrapper_3() { return add(7,8); }
+int wrapper_4() { return fib(add(1,5)); }
