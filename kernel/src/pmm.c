b5e179167d59f5168d66d09291f3adcde61b26a2
tracer-nju <tracer@nju.edu.cn>
2024-01-14 10:38:49 +0800

    Linux LAPTOP-6BH4BGNM 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:38:49 up 31 min,  0 users,  load average: 0.11, 0.19, 0.16

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -4,15 +4,30 @@
 #define HB_MIN (1 << 4)
 #define HB_MAX (1 << 16)
 
+// 这是线段树数组，每一个元素是char类型，占一个字节
+#define HB_HEAD_SIZE (2 * HB_MAX / HB_MIN + 8)
+#define HB_CONT_SIZE (HB_MAX)
+
+static size_t HB_number;   // number of HB
+static void *HB_head_base; // basic addr of HB head
+static void *HB_cont_base; // basic addr of HB content
+
 typedef struct heap_block
 {
-
+  char *head; // 起始头部
+  void *cont; // 内容
+  char stat;  // 信息
 } heap_block;
 
 void kinit()
 {
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
   printf("Heap size: %d\n", pmsize);
+  HB_number = pmsize / (sizeof(heap_block) + HB_HEAD_SIZE + HB_CONT_SIZE);
+  printf("Block number : %d\n", HB_number);
+  HB_head_base = heap.start;
+  HB_cont_base = HB_head_base + sizeof(heap_block) * HB_number;
+  printf("HB_head_base : %p \t HB_cont_base : %p\n", HB_head_base, HB_cont_base);
 }
 
 static void *kalloc(size_t size)
