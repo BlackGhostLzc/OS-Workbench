49d8448da3b16677cf4dfe79749548752ad0176a
tracer-nju <tracer@nju.edu.cn>
2024-01-14 14:49:41 +0800

    Linux LAPTOP-6BH4BGNM 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:49:41 up  4:42,  0 users,  load average: 0.38, 0.19, 0.15

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -87,7 +87,7 @@ static void pmm_init()
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
   printf("%p %p\n", heap.start, (uintptr_t)heap.start);
   printf("%p %p\n", heap.end, (uintptr_t)heap.end);
-  printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
+  printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, (uintptr_t)heap.start, (uintptr_t)heap.end);
   kinit();
 }
 
