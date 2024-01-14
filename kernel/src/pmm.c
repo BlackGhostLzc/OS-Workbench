42308212190513692425bf9c0f67dd41888bff36
tracer-nju <tracer@nju.edu.cn>
2024-01-14 10:23:00 +0800

    Linux LAPTOP-6BH4BGNM 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:23:00 up 15 min,  0 users,  load average: 0.44, 0.15, 0.08

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -1,19 +1,38 @@
 #include <common.h>
+#include <inttypes.h>
 
-static void *kalloc(size_t size) {
+#define HB_MIN (1 << 4)
+#define HB_MAX (1 << 16)
+
+typedef struct heap_block
+{
+
+} heap_block;
+
+void kinit()
+{
+  uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
+  printf("Heap size: %" PRIuPTR "\n", pmsize);
+}
+
+static void *kalloc(size_t size)
+{
   return NULL;
 }
 
-static void kfree(void *ptr) {
+static void kfree(void *ptr)
+{
 }
 
-static void pmm_init() {
+static void pmm_init()
+{
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
+  kinit();
 }
 
 MODULE_DEF(pmm) = {
-  .init  = pmm_init,
-  .alloc = kalloc,
-  .free  = kfree,
+    .init = pmm_init,
+    .alloc = kalloc,
+    .free = kfree,
 };
