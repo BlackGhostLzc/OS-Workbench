71d96dfe1598df305454565d9070547f496c5795
tracer-nju <tracer@nju.edu.cn>
2024-01-14 13:55:33 +0800

    Linux LAPTOP-6BH4BGNM 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:55:33 up  3:48,  0 users,  load average: 0.08, 0.09, 0.09

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -3,36 +3,69 @@
 
 #define HB_MIN (1 << 4)
 #define HB_MAX (1 << 16)
+#define MB (1 << 20)
 
 // 这是线段树数组，每一个元素是char类型，占一个字节
-#define HB_HEAD_SIZE (2 * HB_MAX / HB_MIN + 8)
+#define HB_ARRAY_SIZE (2 * HB_MAX / HB_MIN + 1)
 #define HB_CONT_SIZE (HB_MAX)
 
-static size_t HB_number;   // number of HB
-static void *HB_head_base; // basic addr of HB head
-static void *HB_cont_base; // basic addr of HB content
+/*
+     | ------ heap_block --------| | ------array-------||-------content--------|
+*/
+
+static size_t HB_number;     // number of HB
+static void *HB_struct_base; // basic addr of HB head
+static void *HB_array_base;  // basic addr of HB content
+static void *HB_cont_base;
 
 typedef struct heap_block
 {
-  char *head; // 起始头部
-  void *cont; // 内容
-  char stat;  // 信息
+  char *array; // 起始头部
+  void *cont;  // 内容
+  char stat;
 } heap_block;
+
+void hb_init(struct heap_block *hb, void *start)
+{
+}
 
 void kinit()
 {
-  printf("HB_HEAD size : %d\n", HB_HEAD_SIZE);
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
   printf("Heap size: %d\n", pmsize);
-  HB_number = pmsize / (sizeof(heap_block) + HB_HEAD_SIZE + HB_CONT_SIZE);
+  HB_number = pmsize / (sizeof(heap_block) + HB_ARRAY_SIZE + HB_CONT_SIZE);
   printf("Block number : %d\n", HB_number);
-  HB_head_base = heap.start;
-  HB_cont_base = HB_head_base + sizeof(heap_block) * HB_number;
-  printf("HB_head_base : %p \t HB_cont_base : %p\n", HB_head_base, HB_cont_base);
+  HB_struct_base = heap.start;
+  HB_array_base = heap.start + HB_number * sizeof(struct heap_block);
+  HB_cont_base = HB_array_base + HB_number * HB_ARRAY_SIZE;
+  printf("HB_struct_base : %p \t HB_array_base : %p \t HB_cont_base : %p\n", HB_struct_base, HB_array_base, HB_cont_base);
+
+  for (int i = 0; i < HB_number; i++)
+  {
+  }
+}
+
+// 先把 size 转为 2 的幂
+size_t conv2pow(size_t size)
+{
+  if (size <= 16)
+  {
+    return 16;
+  }
+  size_t res = 16;
+  while (res < size)
+  {
+    res *= 2;
+  }
+  return res;
 }
 
 static void *kalloc(size_t size)
 {
+  size = conv2pow(size);
+  if (size >= 16 * MB)
+    return NULL;
+
   return NULL;
 }
 
