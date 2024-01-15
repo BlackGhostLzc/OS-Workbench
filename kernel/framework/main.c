d186ada292146559650739c091ee01e50630f0f9
tracer-nju <tracer@nju.edu.cn>
2024-01-15 14:25:43 +0800

    Linux LAPTOP-6BH4BGNM 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:25:43 up  5:26,  0 users,  load average: 0.98, 0.77, 0.50

--- kernel/framework/main.c
+++ kernel/framework/main.c
@@ -1,12 +1,15 @@
-
 #include <kernel.h>
 #include <klib.h>
+
+void pmm_test();
 
 int main()
 {
   ioe_init();
   // cte_init(os->trap);
   os->init();
-  mpe_init(os->run);
+
+  // mpe_init(os->run);
+  mpe_init(pmm_test);
   return 1;
 }
