77565176dcc7bdc8bec8df83d59a1e16dfc5d8da
tracer-nju <tracer@nju.edu.cn>
2024-01-16 12:45:02 +0800

    Linux LAPTOP-6BH4BGNM 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:45:02 up  3:10,  0 users,  load average: 0.51, 0.33, 0.18

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -10,9 +10,10 @@ int main(int argc, char *argv[])
       NULL,
   };
   char *exec_envp[] = {
+      "PATH=/bin",
       NULL,
   };
-  execve("/bin/strace", exec_argv, NULL);
+  execve("strace", exec_argv, exec_envp);
   // execve("/bin/strace",     exec_argv, exec_envp);
   // execve("/usr/bin/strace", exec_argv, exec_envp);
   perror(argv[0]);
