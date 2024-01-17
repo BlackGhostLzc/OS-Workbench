d27b9451991aa97eea3c9fa828da63483ab2b1df
tracer-nju <tracer@nju.edu.cn>
2024-01-17 10:03:27 +0800

    Linux LAPTOP-6BH4BGNM 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:03:27 up 24 min,  0 users,  load average: 0.49, 0.32, 0.25

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -2,20 +2,43 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+extern char **__environ;
+char *file_name;
+char **child_argv;
+
+void init_childargv()
+{
+  child_argv[0] = "/bin/strace";
+  child_argv[1] = "-o";
+  child_argv[3] = "-T";
+}
+
+void child()
+{
+  // strace -o filename -T ls -a
+  init_childargv();
+  execve("/bin/strace", child_argv, __environ);
+}
+
+int pipefd[2];
+
+// ./sperf ls -a
 int main(int argc, char *argv[])
 {
-  char *exec_argv[] = {
-      "/usr/bin/strace",
-      "ls",
-      NULL,
-  };
-  char *exec_envp[] = {
-      "PATH=/bin",
-      NULL,
-  };
-  execve("/bin/strace", exec_argv, NULL);
-  // execve("/bin/strace",     exec_argv, exec_envp);
-  // execve("/usr/bin/strace", exec_argv, exec_envp);
-  perror(argv[0]);
-  exit(EXIT_FAILURE);
+  child_argv = (char **)malloc((argc + 3) * sizeof(char *));
+
+  if (pipe(pipefd) < 0)
+  {
+    printf("Error when initializing pipe\n");
+    exit(0);
+  }
+  pid_t pid = fork();
+  if (pid == 0)
+  {
+    child();
+  }
+  else
+  {
+    parent();
+  }
 }
