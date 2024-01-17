ad3bea839fec99c1b24b8c51f2b2b31b85cc504e
tracer-nju <tracer@nju.edu.cn>
2024-01-17 11:09:27 +0800

    Linux LAPTOP-6BH4BGNM 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:09:27 up  1:30,  0 users,  load average: 0.87, 1.04, 0.79

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -46,6 +46,7 @@ void child()
   printf("%s \n", child_argv[3]);
   printf("%s \n", child_argv[4]);
   printf("%s \n", child_argv[5]);
+  printf("%s \n", child_argv[6]);
   execve("/bin/strace", child_argv, __environ);
 
   printf("Should never get here\n");
