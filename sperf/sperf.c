cc4642ceef143c65bc3d5ee7cb9751c4fccaee7c
tracer-nju <tracer@nju.edu.cn>
2024-01-17 10:38:37 +0800

    Linux LAPTOP-6BH4BGNM 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:38:37 up 59 min,  0 users,  load average: 0.35, 0.33, 0.24

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -8,11 +8,14 @@ char **child_argv;
 
 int pipefd[2];
 
-void init_childargv()
+// /bin/strace -o file_path -T ls -a
+void init_childargv(char *argv[])
 {
   child_argv[0] = "/bin/strace";
   child_argv[1] = "-o";
   child_argv[3] = "-T";
+
+  // child_argv[4] = argv[1]    child_argv[5] = argv[2]
 }
 
 void child()
@@ -21,10 +24,10 @@ void child()
   close(pipefd[0]);
 
   // strace -o filename -T ls -a
-  init_childargv();
   char file_path[64];
   // /proc/pid/fd/ 目录下是进程所打开的全部文件描述符，这是个符号链接
   sprintf(file_path, "/proc/%d/fd/%d", getpid(), pipefd[1]);
+
   child_argv[2] = file_path;
   printf("here\n");
   execve("/bin/strace", child_argv, __environ);
@@ -38,7 +41,10 @@ void parent()
 // ./sperf ls -a
 int main(int argc, char *argv[])
 {
+  printf("argc: %d\n", argc);
+
   child_argv = (char **)malloc((argc + 3) * sizeof(char *));
+  init_childargv(argv);
 
   if (pipe(pipefd) < 0)
   {
