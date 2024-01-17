7c85f95ba55d80628b70fed64f9da9426a4f2eec
tracer-nju <tracer@nju.edu.cn>
2024-01-17 11:23:39 +0800

    Linux LAPTOP-6BH4BGNM 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:23:39 up  1:44,  0 users,  load average: 0.68, 0.40, 0.51

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -12,11 +12,8 @@ int pipefd[2];
 void init_childargv(int argc, char *argv[])
 {
   child_argv[0] = "/bin/strace";
-  printf("%s ", child_argv[0]);
   child_argv[1] = "-o";
-  printf("%s ", child_argv[1]);
   child_argv[3] = "-T";
-  printf("%s ", child_argv[3]);
 
   // child_argv[4] = argv[1]    child_argv[5] = argv[2]
   for (int i = 1; i < argc; i++)
