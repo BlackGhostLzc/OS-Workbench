4388e1884df2837f1375266eb4953f0348741b29
tracer-nju <tracer@nju.edu.cn>
2024-01-18 16:11:54 +0800

    Linux LAPTOP-6BH4BGNM 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:11:54 up  6:34,  0 users,  load average: 0.19, 0.36, 0.34

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -193,7 +193,7 @@ void parent()
  */
 
 // 设置终端展示时候的窗口高
-#define SYSCALL_INFO_WINDOW_HEIGHT (25)
+#define SYSCALL_INFO_WINDOW_HEIGHT (20)
 // 设置终端展示时候的窗口宽
 #define SYSCALL_INFO_WINDOW_WIDTH (120)
 
